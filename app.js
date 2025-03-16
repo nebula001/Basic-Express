f5d8bdda89e419f8276ae701a4298b116d4bcb7c
Aameer <officialaameer001@gmail.com>
2025-03-16 22:17:53 +0530

    Added data.js and more routes in app.js

--- app.js
+++ app.js
@@ -2,13 +2,25 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const { products, people } = require("./data.js");
 
-app.get("/", (req, res) => {
-  res.status(200).send(`Hello World`);
+// Middleware to parse JSON
+app.use(express.json());
+
+app.get("/products", (req, res) => {
+  res.status(200).json(products);
 });
 
-app.get("/about", (req, res) => {
-  res.status(200).send(`About Page`);
+app.get("/products/:id", (req, res) => {
+  res.status(200).json(products);
+});
+
+app.get("/people", (req, res) => {
+  res.status(200).json(people);
+});
+
+app.get("/people/:id", (req, res) => {
+  res.status(200).json(people);
 });
 
 app.get("*", (req, res) => {
